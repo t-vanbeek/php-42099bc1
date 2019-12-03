1492f0a40eff14e52698eeb5381eb9ad489636a2
Thorvald van Beek <54794887+t-vanbeek@users.noreply.github.com>
2019-12-03 12:47:50 +0100

    Uitwerking van de exercise

---
+++ index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Index.php</title>
+    </head>
+    <body>
+        <h1>
+            <?php
+            $tekst = "Iets";
+            echo $tekst;
+            ?>
+        </h1>
+    </body>
+</html>
